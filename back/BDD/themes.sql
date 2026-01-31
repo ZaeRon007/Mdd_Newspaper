1268d8e8682561d3147c0fb15aa023657d2f6b87
ZaeRon007 <julien.pelissier8@orange.fr>
2026-01-31 11:14:13 +0100

    implementation of themes example in BDD directory

---
+++ back/BDD/themes.sql
@@ -0,0 +1,6 @@
+INSERT INTO `theme` (`id`, `name`, `content`)
+VALUES
+(1,'Sport','Articles de Sport'),
+(2,'Cuisine','Articles de Cuisine'),
+(3,'Actualités','Articles de Actualités'),
+(4,'Politique','Articles de Politique');

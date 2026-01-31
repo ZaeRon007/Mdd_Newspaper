bbea9c43d355890a2981af32b2826a72cfe1a897
ZaeRon007 <julien.pelissier8@orange.fr>
2026-01-31 16:59:08 +0100

    jest : fix for path search at files

--- front/jest.config.js
+++ front/jest.config.js
@@ -2,4 +2,7 @@ module.exports = {
     preset: 'jest-preset-angular',
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+    moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1'
+  }
 };

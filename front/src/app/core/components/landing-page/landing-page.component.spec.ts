a9e7b5ee2f5b439d869bffd4d01211d4881ae836
ZaeRon007 <julien.pelissier8@orange.fr>
2026-02-02 10:51:34 +0100

    landing page component test

--- front/src/app/core/components/landing-page/landing-page.component.spec.ts
+++ front/src/app/core/components/landing-page/landing-page.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LandingPageComponent } from './landing-page.component';
+import { Router } from '@angular/router';
 
 
 describe('LandingPageComponent', () => {
   let component: LandingPageComponent;
   let fixture: ComponentFixture<LandingPageComponent>;
+
+  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +17,19 @@ describe('LandingPageComponent', () => {
 
     fixture = TestBed.createComponent(LandingPageComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    router = TestBed.inject(Router);
+    jest.spyOn(router, 'navigateByUrl');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to /auth', () => {
+    component.start();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/auth');
   });
 });

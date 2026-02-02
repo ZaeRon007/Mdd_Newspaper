34e55289f07ce5616f8a49ec718d7cb0ef261e98
ZaeRon007 <julien.pelissier8@orange.fr>
2026-02-02 20:07:48 +0100

    create component test

--- front/src/app/mddapi/components/create/create.component.spec.ts
+++ front/src/app/mddapi/components/create/create.component.spec.ts
@@ -7,27 +7,71 @@ import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
 import { FullHeaderComponent } from 'src/app/core/components/headers/full-header/full-header.component';
 import { MatDividerModule } from '@angular/material/divider';
-import { provideRouter, RouterModule } from '@angular/router';
+import { provideRouter, Router, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ArticleService } from '../../services/articlesService';
+import { of } from 'rxjs';
 
 describe('CreateComponent', () => {
   let component: CreateComponent;
   let fixture: ComponentFixture<CreateComponent>;
 
+  let mockArticlesService: any;
+  let router: Router;
+
   beforeEach(async () => {
+
+    mockArticlesService = {
+      createArticle: jest.fn(),
+    }
+
     await TestBed.configureTestingModule({
       declarations: [CreateComponent, FullHeaderComponent],
-      imports:[MatCardModule, MatIconModule, MatSelectModule, MatDividerModule, RouterModule, FormsModule],
-      providers: [provideHttpClient(), provideRouter([])]
-    })
-      .compileComponents();
+      imports: [
+        MatCardModule,
+        MatIconModule,
+        MatSelectModule,
+        MatDividerModule,
+        RouterModule,
+        FormsModule,
+        MatInputModule,
+        BrowserAnimationsModule
+      ],
+      providers: [
+        provideHttpClient(),
+        provideRouter([]),
+        { provide: ArticleService, useValue: mockArticlesService }
+      ]
+    }).compileComponents();
 
     fixture = TestBed.createComponent(CreateComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+  });
+
+  beforeEach(() => {
+    router = TestBed.inject(Router);
+    jest.spyOn(router, 'navigateByUrl');
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should create an article and navigate to /articles/home', () => {
+    const article = {
+      themeId: 1,
+      title: 'title',
+      content: 'content'
+    }
+
+    mockArticlesService.createArticle.mockReturnValue(of(article));
+
+    fixture.detectChanges();
+
+    component.onSubmit();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/articles/home');
+  })
 });

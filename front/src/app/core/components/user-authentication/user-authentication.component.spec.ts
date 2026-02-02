70a6c48b2bca3d106a6b60c90569957a3e88c4be
ZaeRon007 <julien.pelissier8@orange.fr>
2026-02-02 10:48:44 +0100

    user authentication component test

--- front/src/app/core/components/user-authentication/user-authentication.component.spec.ts
+++ front/src/app/core/components/user-authentication/user-authentication.component.spec.ts
@@ -1,23 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UserAuthenticationComponent } from './user-authentication.component';
+import { Router } from '@angular/router';
 
 describe('UserAuthenticationComponent', () => {
   let component: UserAuthenticationComponent;
   let fixture: ComponentFixture<UserAuthenticationComponent>;
 
+  let router: Router;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UserAuthenticationComponent ]
+      declarations: [UserAuthenticationComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(UserAuthenticationComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
+
+  beforeEach(() => {
+    router = TestBed.inject(Router);
+    jest.spyOn(router, 'navigateByUrl');
+  })
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should navigate to /auth/register', () => {
+    component.register();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/auth/register');
+  });
+
+  it('should navigate to /auth/login', () => {
+    component.login();
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/auth/login');
   });
 });

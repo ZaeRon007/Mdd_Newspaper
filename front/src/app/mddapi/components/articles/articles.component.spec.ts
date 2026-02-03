a7be4708456f6f000385d5b1c7b3d8c195b2a8f7
ZaeRon007 <julien.pelissier8@orange.fr>
2026-02-03 12:19:02 +0100

    missing test in articles component

--- front/src/app/mddapi/components/articles/articles.component.spec.ts
+++ front/src/app/mddapi/components/articles/articles.component.spec.ts
@@ -88,4 +88,49 @@ describe('ArticlesComponent', () => {
     expect(titles[1]!.textContent).toContain('Article test1');
     expect(titles[2]!.textContent).toContain('Article test2');
   })
+
+  it('sould sort articles ascendant then descendant', () => {
+
+    const mockArticlesResponse = [
+      {
+        id: 1,
+        title: 'Article test1',
+        date: '02/02/2026',
+        user: '1',
+        content: 'ceci est le contenu de l article test',
+        theme: 'test'
+      },
+      {
+        id: 2,
+        title: 'Article test2',
+        date: '01/02/2026',
+        user: '1',
+        content: 'ceci est le contenu de l article test 2',
+        theme: 'test'
+      },
+      {
+        id: 3,
+        title: 'Article test3',
+        date: '03/02/2026',
+        user: '1',
+        content: 'ceci est le contenu de l article test 3',
+        theme: 'test'
+      }
+    ]
+
+    mockArticlesService.setupArticles.mockReturnValue(of(mockArticlesResponse));
+
+    fixture.detectChanges();
+
+    expect(component.articles.at(0)?.date).toBe('03/02/2026');
+    expect(component.articles.at(1)?.date).toBe('02/02/2026');
+    expect(component.articles.at(2)?.date).toBe('01/02/2026');
+
+    component.sortByDate();
+
+    expect(component.articles.at(0)?.date).toBe('01/02/2026');
+    expect(component.articles.at(1)?.date).toBe('02/02/2026');
+    expect(component.articles.at(2)?.date).toBe('03/02/2026');
+
+  })
 });

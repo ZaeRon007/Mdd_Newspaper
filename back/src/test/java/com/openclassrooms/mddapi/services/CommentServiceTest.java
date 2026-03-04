60b005a5a06b62766dfbcd2a0677c758a6dd423a
ZaeRon007 <julien.pelissier8@orange.fr>
2026-03-04 17:09:15 +0100

    comment service test implementation

---
+++ back/src/test/java/com/openclassrooms/mddapi/services/CommentServiceTest.java
@@ -0,0 +1,56 @@
+package com.openclassrooms.mddapi.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.openclassrooms.mddapi.model.CommentEntity;
+import com.openclassrooms.mddapi.model.dto.CommentDto;
+import com.openclassrooms.mddapi.model.dto.UserDto;
+import com.openclassrooms.mddapi.repository.CommentRepository;
+
+@Tag("CommentService")
+@DisplayName("unit test for CommentService")
+@SpringBootTest
+@ActiveProfiles("test")
+public class CommentServiceTest {
+    
+    @InjectMocks
+    private CommentService commentService;
+
+    @Mock
+    private UserService userService;
+
+    @Mock
+    private CommentRepository commentRepository;
+
+    @Test
+    @DisplayName("should comment article")
+    public void shouldCommentArticle() {
+        String content = "content", username = "name";
+
+        UserDto user = new UserDto();
+        user.setId(0);
+        user.setEmail("email");
+        user.setName(username);
+
+        when(userService.getMe()).thenReturn(user);
+        when(commentRepository.save(any(CommentEntity.class))).thenReturn(null);
+
+        CommentDto commentDto = commentService.commentArticle(1, content);
+
+        assertNotNull(commentDto);
+        assertEquals(commentDto.getContent(), content);
+        assertEquals(commentDto.getUser(), username);
+
+    }
+}

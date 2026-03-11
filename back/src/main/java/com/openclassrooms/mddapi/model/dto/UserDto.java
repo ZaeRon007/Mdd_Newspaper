b85b4d0cd681da415cbfe431a1adc1007afa9cd8
ZaeRon007 <julien.pelissier8@orange.fr>
2026-03-11 15:01:39 +0100

    addition of @NoArgsConstructor and @AllArgsConstructor in userDto and UserRegisterAndLoginDto

--- back/src/main/java/com/openclassrooms/mddapi/model/dto/UserDto.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/dto/UserDto.java
@@ -2,22 +2,17 @@ package com.openclassrooms.mddapi.model.dto;
 
 import com.openclassrooms.mddapi.model.UserEntity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDto {
     int id;
     String name;
     String email;
-
-    public UserDto(){
-    }
-
-    public UserDto(int id, String name, String email){
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
 
     public UserEntity toUserEntity(){
         UserEntity userEntity = new UserEntity();

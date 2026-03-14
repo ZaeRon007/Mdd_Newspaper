5d2e9574c7d63364a550ea82c81a58ab178816f7
ZaeRon007 <julien.pelissier8@orange.fr>
2026-03-14 23:51:54 +0100

    h2 database intialization for user controller integration test quality upgrade

---
+++ back/src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO `theme` (`id`, `name`, `content`)
+VALUES
+(1,'Sport','Articles de Sport'),
+(2,'Cuisine','Articles de Cuisine'),
+(3,'Actualités','Articles de Actualités'),
+(4,'Politique','Articles de Politique');
+
+INSERT INTO `users` (`id`,`name`,`email`,`password`,`created_at`,`updated_at`)
+VALUES
+(0,'pedro','pedro@gmail.com','$2a$10$9Ln4wYxk1zC3oXk4RUR/VO7aaDyMdw1CC2Z1fRkJRv5SfBxVDxSjK','2026-02-02 17:25:19','2026-02-02 17:25:19');
--- back/src/test/java/com/openclassrooms/mddapi/controllers/UserControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/mddapi/controllers/UserControllerIntegrationTest.java
@@ -23,89 +23,87 @@ import com.jayway.jsonpath.JsonPath;
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Tag("User Controller")
+@Tag("UserControllerIntegrationTest")
 @ActiveProfiles("test")
 @TestInstance(Lifecycle.PER_CLASS)
 public class UserControllerIntegrationTest {
-    @Autowired
-    private MockMvc mockMvc;
+        @Autowired
+        private MockMvc mockMvc;
 
-    private String token;
+        private String token;
 
-    @BeforeAll
-    @DisplayName("should register user")
-    public void shouldRegisterUser() throws Exception {
-        String authRequest = "{" +
-                "\"name\": \"name\"," +
-                "\"email\": \"email@gmail.com\"," +
-                "\"password\": \"superPassword\"" +
-                "}";
+        @BeforeAll
+        @DisplayName("should login user")
+        public void shouldLoginUser() throws Exception {
+                String authRequest = "{" +
+                                "\"email\": \"pedro@gmail.com\"," +
+                                "\"password\": \"test1234!A\"" +
+                                "}";
+                MvcResult response = mockMvc.perform(post("/api/auth/login")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(authRequest))
+                                .andExpect(status().isOk())
+                                .andReturn();
 
-        MvcResult response = mockMvc.perform(post("/api/auth/register")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(authRequest))
-                .andExpect(status().isOk())
-                .andReturn();
+                String tokenResponse = response.getResponse().getContentAsString();
+                token = JsonPath.parse(tokenResponse).read("$.token");
+        }
 
+        @Test
+        @DisplayName("should register user")
+        public void shouldRegisterUser() throws Exception {
+                String authRequest = "{" +
+                                "\"name\": \"name\"," +
+                                "\"email\": \"email@gmail.com\"," +
+                                "\"password\": \"superPassword\"" +
+                                "}";
 
-        String tokenResponse = response.getResponse().getContentAsString();
-        token = JsonPath.parse(tokenResponse).read("$.token");
-    }
+                mockMvc.perform(post("/api/auth/register")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(authRequest))
+                                .andExpect(status().isOk());
+        }
 
-    @Test
-    @DisplayName("should login user")
-    public void shouldLoginUser() throws Exception {
-        String authRequest = "{" +
-                "\"email\": \"email@gmail.com\"," +
-                "\"password\": \"superPassword\"" +
-                "}";
-        mockMvc.perform(post("/api/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(authRequest))
-                .andExpect(status().isOk())
-                .andReturn();
-    }
+        @Test
+        @DisplayName("should return logged user")
+        public void shouldReturnLoggedUser() throws Exception {
+                mockMvc.perform(get("/api/auth/me")
+                                .header("Authorization", "Bearer " + token)
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.id").value(0))
+                                .andExpect(jsonPath("$.name").value("pedro"))
+                                .andExpect(jsonPath("$.email").value("pedro@gmail.com"));
+        }
 
-    @Test
-    @DisplayName("should return logged user")
-    public void shouldReturnLoggedUser() throws Exception {
-        mockMvc.perform(get("/api/auth/me")
-                .header("Authorization", "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.name").value("name"))
-                .andExpect(jsonPath("$.email").value("email@gmail.com"));
-    }
+        @AfterAll
+        @DisplayName("should return put user")
+        public void shouldReturnPutUser() throws Exception {
+                String body = "{" +
+                                "\"id\": 1," +
+                                "\"name\": \"anotherName\"," +
+                                "\"email\": \"email@yahoo.com\"" +
+                                "}";
 
-    @AfterAll
-    @DisplayName("should return put user")
-    public void shouldReturnPutUser() throws Exception {
-        String body = "{" +
-                "\"id\": 1," +
-                "\"name\": \"anotherName\"," +
-                "\"email\": \"email@yahoo.com\"" +
-                "}";
+                mockMvc.perform(put("/api/profile/me")
+                                .header("Authorization", "Bearer " + token)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(body))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.id").value(1))
+                                .andExpect(jsonPath("$.name").value("anotherName"))
+                                .andExpect(jsonPath("$.email").value("email@yahoo.com"));
+        }
 
-        mockMvc.perform(put("/api/profile/me")
-                .header("Authorization", "Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(body))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.name").value("anotherName"))
-                .andExpect(jsonPath("$.email").value("email@yahoo.com"));
-    }
-
-    @Test
-    @DisplayName("should found user by its id")
-    public void shouldFoundUserById() throws Exception{
-        mockMvc.perform(get("/api/user/" + 1)
-                .header("Authorization","Bearer " + token)
-                .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.id").value(1))
-                .andExpect(jsonPath("$.name").value("name"))
-                .andExpect(jsonPath("$.email").value("email@gmail.com"));
-    }
+        @Test
+        @DisplayName("should found user by its id")
+        public void shouldFoundUserById() throws Exception {
+                mockMvc.perform(get("/api/user/" + 0)
+                                .header("Authorization", "Bearer " + token)
+                                .contentType(MediaType.APPLICATION_JSON))
+                                .andExpect(status().isOk())
+                                .andExpect(jsonPath("$.id").value(0))
+                                .andExpect(jsonPath("$.name").value("pedro"))
+                                .andExpect(jsonPath("$.email").value("pedro@gmail.com"));
+        }
 }

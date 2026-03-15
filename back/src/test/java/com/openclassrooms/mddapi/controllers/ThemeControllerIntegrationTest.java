2a2ea252a8d714104e71f458451095bf93fa623d
ZaeRon007 <julien.pelissier8@orange.fr>
2026-03-15 13:13:29 +0100

    minor ugrade for integration test in theme controller

--- back/src/test/java/com/openclassrooms/mddapi/controllers/ThemeControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/mddapi/controllers/ThemeControllerIntegrationTest.java
@@ -5,7 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -33,8 +33,7 @@ public class ThemeControllerIntegrationTest {
 
     private String token;
 
-    @BeforeAll
-    @Test
+    @BeforeEach
     @DisplayName("should login user")
     public void shouldLoginUser() throws Exception {
         String authRequest = "{" +

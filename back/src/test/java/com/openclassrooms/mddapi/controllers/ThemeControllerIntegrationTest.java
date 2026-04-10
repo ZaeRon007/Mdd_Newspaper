15e05843e4a28ac742255153b86ad190226ed2db
ZaeRon007 <julien.pelissier8@orange.fr>
2026-04-10 16:23:33 +0200

    upgrade for theme controller integration test. Implementation of mock user with @WithMockUser annotation

--- back/src/test/java/com/openclassrooms/mddapi/controllers/ThemeControllerIntegrationTest.java
+++ back/src/test/java/com/openclassrooms/mddapi/controllers/ThemeControllerIntegrationTest.java
@@ -1,11 +1,9 @@
 package com.openclassrooms.mddapi.controllers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -15,11 +13,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
-
-import com.jayway.jsonpath.JsonPath;
 
 @SpringBootTest
 @Tag("themeControllerIntegrationTest")
@@ -31,39 +27,21 @@ public class ThemeControllerIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private String token;
-
-    @BeforeEach
-    @DisplayName("should login user")
-    public void shouldLoginUser() throws Exception {
-        String authRequest = "{" +
-                "\"email\": \"pedro@gmail.com\"," +
-                "\"password\": \"test1234!A\"" +
-                "}";
-        MvcResult response = mockMvc.perform(post("/api/auth/login")
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(authRequest))
-                .andExpect(status().isOk())
-                .andReturn();
-
-        String tokenResponse = response.getResponse().getContentAsString();
-        token = JsonPath.parse(tokenResponse).read("$.token");
-    }
 
     @Test
     @DisplayName("should return themes")
+    @WithMockUser(username = "pedro@gmail.com")
     public void shouldGetThemes() throws Exception {
         mockMvc.perform(get("/api/themes")
-                .header("Authorization", "Bearer " + token)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk());
     }
 
     @Test
     @DisplayName("should return a theme by its id")
+    @WithMockUser(username = "pedro@gmail.com")
     public void shouldGetThemeById() throws Exception{
         mockMvc.perform(get("/api/theme/" + 1)
-                .header("Authorization", "Bearer " + token)
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.name").value("Sport"))

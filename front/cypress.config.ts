ee0478590b2fdc69bd0e723a69ed2868d87a75bf
ZaeRon007 <julien.pelissier8@orange.fr>
2026-04-20 10:00:38 +0200

    fix for cypress configuration in cypress config ts

--- front/cypress.config.ts
+++ front/cypress.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  allowCypressEnv: false,
+  component: {
+    devServer: {
+      framework: "angular",
+      bundler: "webpack",
+    },
+    specPattern: "**/*.cy.ts",
+  },
 
   e2e: {
     setupNodeEvents(on, config) {

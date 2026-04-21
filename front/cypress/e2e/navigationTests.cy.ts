cd56d6f0a7753cdc546fb6ed4af80eb7b251263c
ZaeRon007 <julien.pelissier8@orange.fr>
2026-04-21 11:49:53 +0200

    update for unguard routes

--- front/cypress/e2e/navigationTests.cy.ts
+++ front/cypress/e2e/navigationTests.cy.ts
@@ -3,17 +3,36 @@ describe('navigationTests', () => {
     it('should test all navigations possibilities', () => {
         cy.loginUser('test', 'test1234!A');
         
+        
+        cy.get('a').contains('Articles').click();
+        cy.url().should('include', '/articles/home');
+        
+        cy.get('a').contains('Thèmes').click();
+        cy.url().should('include', '/articles/themes');
+        
+        cy.get('img[alt="mddapi_logo"]').click();
+        cy.url().should('include', '/articles/home');
+        
         cy.get('mat-icon').contains('account_circle').click();
         cy.url().should('include', '/profile/me');
 
-        cy.get('a').contains('Articles').click();
-        cy.url().should('include', '/articles/home');
+        cy.get('span').contains('Se déconnecter').click();
+        cy.get('h1').contains('Welcome to MDD');
 
-        cy.get('a').contains('Thèmes').click();
-        cy.url().should('include', '/articles/themes');
+        cy.get('span').contains('Commencer').click();
+        cy.url().should('include', '/auth');
 
-        cy.get('img[alt="mddapi_logo"]').click();
-        cy.url().should('include', '/articles/home');
+        cy.get('button').contains('Se connecter').click();
+        cy.url().should('include', '/auth/login');
+        cy.get('mat-icon').contains('arrow_back').click();
+
+        cy.url().should('include', '/auth');
+        cy.get('button').contains('S\'inscrire').click();
+        cy.url().should('include', '/auth/register');
+        cy.get('mat-icon').contains('arrow_back').click();
+
+        cy.url().should('include', '/auth');
+
 
     })
 
